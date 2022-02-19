fe6db1c5967ec6f34ce3d600e59efa02ae01080c
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2022-02-19 12:19:19 -0300

    chore: list projects

--- src/components/siteEdit/Jobs/index.vue
+++ src/components/siteEdit/Jobs/index.vue
@@ -1,7 +1,55 @@
-<template>Experiência</template>
+<template>
+  <div v-if="jobs" class="jobs">
+    <div class="flex items-center justify-between">
+      <h1 class="jobs__title">Editar experiências</h1>
 
-<script>
-export default {
-  name: "SiteJobs",
+      <button class="jobs__button">atualizar</button>
+    </div>
+    <div class="jobs__list">
+      <div v-for="job in jobs" :key="job.objectID" class="jobs__list__group">
+        image: {{ job.image }} <br />
+        description: {{ job.description }}<br />
+        name: {{ job.name }} <br />
+        ocupation: {{ job.ocupation }}<br />
+        period: {{ job.period }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { getJobs } from "../../../services/algolia";
+import { ref } from "vue";
+
+const jobs = ref(null);
+
+const get = async () => {
+  try {
+    const { data, status } = await getJobs();
+    if (data && status === 200) jobs.value = data.hits;
+    console.log(jobs.value);
+  } catch (error) {
+    console.log(error);
+  }
 };
+get();
 </script>
+
+<style lang="scss" scoped>
+.jobs {
+  &__title {
+    @apply text-2xl  text-gray-600 font-bold font-sans;
+  }
+
+  &__list {
+    @apply overflow-auto mt-4;
+    height: 500px;
+    &__group {
+      @apply w-full my-5;
+    }
+  }
+  &__button {
+    @apply bg-green-700 rounded-lg px-4 py-2 text-lg text-white tracking-wide font-semibold font-sans;
+  }
+}
+</style>
--- src/services/algolia.js
+++ src/services/algolia.js
@@ -3,6 +3,10 @@ import config from "./config";
 export const getAbout = async () => await config.get("/about");
 
 export const updateAbout = async (data) =>
-  await config.put(`/about/${data.objectID}`, {
+  await config.put(`/about/settings`, {
     data,
   });
+
+export const getJobs = async () => await config.get("/jobs");
+
+export const getprojects = async () => await config.get("/projects");
