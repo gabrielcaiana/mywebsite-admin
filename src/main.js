e4aa394581d12d47098777d48d8b56b40299f010
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2022-03-01 15:10:22 -0300

    install sentry

--- src/main.js
+++ src/main.js
@@ -4,4 +4,34 @@ import "./scss/index.scss";
 import { createPinia } from "pinia";
 import router from "./router";
 
-createApp(App).use(createPinia()).use(router).mount("#app");
+import * as Sentry from "@sentry/vue";
+import { BrowserTracing } from "@sentry/tracing";
+
+const app = createApp(App);
+
+Sentry.init({
+  app,
+  dsn: "https://36b24830a62d401f91af985a033ef61e@o1127381.ingest.sentry.io/6169238",
+  logErrors: true,
+  // eslint-disable-next-line no-undef
+  release: __SENTRY_RELEASE__,
+  environment: import.meta.env.MODE,
+  integrations: [
+    new BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      tracingOrigins: ["localhost", "my-site-url.com", /^\//],
+    }),
+  ],
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 0.2,
+});
+
+app.use(createPinia()).use(router).mount("#app");
+
+const user = {
+  email: "gabrielcaianaguedes@gmail.com",
+};
+Sentry.setUser(user);
+Sentry.configureScope((scope) => scope.setUser(null));
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,38 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import eslintPlugin from "vite-plugin-eslint";
+import viteSentry from "vite-plugin-sentry";
+
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, ".env.local") });
+
+const viteSentryConfig = {
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+  org: "gabrielcaiana",
+  project: "gabrielcaiana",
+  release: `${process.env.npm_package_name}@${process.env.npm_package_version}`,
+  setCommits: {
+    auto: true,
+  },
+  sourceMaps: {
+    include: ["./dist/assets"],
+    ignore: ["node_modules"],
+    urlPrefix: "~/assets",
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), eslintPlugin()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  plugins: [vue(), viteSentry(viteSentryConfig), eslintPlugin()],
+  build: {
+    sourcemap: "hidden",
+  },
+  define: {
+    __SENTRY_RELEASE__: `"${process.env.npm_package_name}@${process.env.npm_package_version}"`,
+  },
 });
